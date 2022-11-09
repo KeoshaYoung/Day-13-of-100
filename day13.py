665a5236874502a2a26c833efc66c7396cc0cb29
Keosha Young <kyoung1012@gmail.com>
2022-11-09 11:19:27 -0500

    Add files via upload

--- day13.py
+++ day13.py
@@ -3,15 +3,15 @@ name = input("Name of the exam:")
 maxScore = int(input("Max. Poss. Score:"))
 score = float(input("Your score:"))
 score = round(score, 2)
-if score >= 90.00 or score <= 100.00:
+if score >= 90.00:
     print("You got", score, ", which is an A+")
-elif score <= 80.00 or score <= 89.00:
+elif score >= 80.00:
     print("You got", score, ", which is an A")
-elif score >= 70.00 or score <= 79.00:
+elif score >= 70.00:
     print("You got", score, ", which is a B")
-elif score >= 60.00 or score <= 69.00:
+elif score >= 60.00:
     print("You got", score, ", which is a C")
-elif score >= 50.00 or score <= 59.00:
+elif score >= 50.00:
     print("You got", score, ", which is a D")
 else:
     print("You got", score, ", which is a U")
